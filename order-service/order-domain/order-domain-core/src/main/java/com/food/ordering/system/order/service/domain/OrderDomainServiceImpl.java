a65be708e0e9af02708ab1d450e3ee0d693f4738
aymansalem22 <s.aymansalem@gmail.com>
2023-04-12 03:59:10 +0200

    Implementing Order Domain Service & Implementing Order Application Service - DTO classes

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,115 @@
+package com.food.ordering.system.order.service.domain;
+
+import com.food.ordering.system.domain.valueobject.ProductId;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import com.food.ordering.system.order.service.domain.entity.Product;
+import com.food.ordering.system.order.service.domain.entity.Restaurant;
+import com.food.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.food.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.food.ordering.system.order.service.domain.event.OrderPaidEvents;
+import com.food.ordering.system.order.service.domain.exception.OrderDomainException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+@Slf4j
+public class OrderDomainServiceImpl implements OrderDomainService{
+
+    private static final String UTC="UTC";
+    @Override
+    public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+       validateRestaurant(restaurant);
+       setOrderProductInformation(order,restaurant);
+       order.validateOrder();
+       order.intializeOrder();
+       log.info("Order with id: {} is initiated",order.getId().getValue());
+       return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+
+
+    @Override
+    public OrderPaidEvents payOrder(Order order) {
+       order.pay();
+       log.info("Order with id: {} is paid",order.getId().getValue());
+       return new OrderPaidEvents(order,ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+        order.approve();
+        log.info("Order with id: {} is approved",order.getId().getValue());
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        order.initCancel(failureMessages);
+        log.info("Order payment is cancelling for order id: {}",order.getId().getValue());
+        return new OrderCancelledEvent(order,ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+      order.cancel(failureMessages);
+      log.info("Order with id: {} is cancelled",order.getId().getValue());
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if(!restaurant.isActive()){
+            throw new OrderDomainException("Restaurant with id "+restaurant.getId().getValue()+
+                    " is currently not active!");
+        }
+
+    }
+
+    //there are two ways below with hashmap or stream alternative to use this method!!
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        order.getItems().forEach(orderItem -> restaurant.getProducts().forEach(restaurantProduct ->{
+                Product currentProduct= orderItem.getProduct();
+        if(currentProduct.equals(restaurantProduct)){
+        currentProduct.updateWithConfirmedNameAndPrice(restaurantProduct.getName(),
+                restaurantProduct.getPrice());
+        }
+                }
+        ));
+
+    }
+
+    //first alter
+    private void setOrderProductInformationWithHashMap(Order order, Restaurant restaurant) {
+      Map<ProductId,Product> restaurantProductMap=new HashMap<>();
+      restaurant.getProducts().forEach(restaurantProduct ->{
+          restaurantProductMap.put(restaurantProduct.getId(),restaurantProduct);
+      });
+      order.getItems().forEach(orderItem -> {
+          Product currentProduct=orderItem.getProduct();
+          Product restaurantProduct=restaurantProductMap.get(currentProduct.getId());
+          currentProduct.updateWithConfirmedNameAndPrice(restaurantProduct.getName(),
+                  restaurantProduct.getPrice());
+      });
+
+    }
+
+    //second alter
+    private void setOrderProductInformationWithStream(Order order, Restaurant restaurant) {
+        Map<ProductId, Product> restaurantProductMap = restaurant.getProducts().stream().
+                collect(Collectors.toMap(Product::getId, Function.identity()));
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
